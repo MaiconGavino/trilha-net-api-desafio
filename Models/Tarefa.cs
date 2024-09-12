ebb0b8a3bba958c2df14d7931fc4ef964b4b6276
maicongavino <maiconserrao57@gmail.com>
2024-09-12 13:43:12 -0400

    Desafio Concluido

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,16 @@ namespace TrilhaApiDesafio.Models
     public class Tarefa
     {
         public int Id { get; set; }
+
+        [Column(TypeName = "varchar(255)")] // Configurar Titulo como varchar(255)
         public string Titulo { get; set; }
+
+        [Column(TypeName = "text")] // Configurar Descricao como TEXT
         public string Descricao { get; set; }
+
         public DateTime Data { get; set; }
+
         public EnumStatusTarefa Status { get; set; }
     }
+
 }
